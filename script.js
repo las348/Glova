8d67950a39ef9b649fd33d909e933ec8ba8802b3
Laureni Wilkinson <las348@nyu.edu>
2020-05-08 12:33:54 -0400

    youtube-search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-
-    //Edamam- 
+    var APIKey = "AIzaSyCJSImhpn0LjuUOCoOW2gbZqEB4KxMeguU";  //youtube key
+    //Edamam
     var searchInput = document.getElementById("search-input").textContent;
 
     $("button").on("click", function () {
@@ -38,7 +38,6 @@ $(document).ready(function () {
                 var cardText3 = $("<a>").text("Recipe").attr("href", recipeURL).addClass("list-group-item");
 
 
-
                 cardDiv.append(image);
                 cardDiv.append(cardBodyDiv);
                 cardBodyDiv.append(cardTitle);
@@ -54,7 +53,7 @@ $(document).ready(function () {
             }
             $("#search-container").addClass("search-container");
             $("#main-container").removeClass("main-container").addClass("main-container-2")
-            
+
             console.log(response);
 
 
@@ -63,11 +62,48 @@ $(document).ready(function () {
         function clear() {
             $("#recipe-view").empty();
         }
+        //video search results
+        searchResults();
+            
+        function searchResults() {
+
+            var youtubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=05&type=video&key=" + APIKey + "&q=" + searchInput;
+
+            $.ajax({
+                url: youtubeURL,
+                method: "GET"
+            }).then(function (response) {
+
+                // Log the resulting object
+                console.log(response);
+                var resultVid = response.items[0].id.videoId;
+                var resultTitle = response.items[0].snippet.title;
+
+                var resultVid2 = response.items[1].id.videoId;
+                var resultTitle2 = response.items[1].snippet.title;
+
+                var resultVid3 = response.items[2].id.videoId;
+                var resultTitle3 = response.items[2].snippet.title;
+
+
+                $("#heading").addClass("hide");
+
+                $("#frame").attr("src", "https://www.youtube.com/embed/" + resultVid);
+                $("#vid-desc").text(resultTitle);
+
+                $("#frame2").attr("src", "https://www.youtube.com/embed/" + resultVid2);
+                $("#vid-desc2").text(resultTitle2);
+
+                $("#frame3").attr("src", "https://www.youtube.com/embed/" + resultVid3);
+                $("#vid-desc3").text(resultTitle3);
+
+
+            }); //vid search response
+        }; //searchResults function
 
     });
 
-
-    //Youtube-Laureni
+    //Youtube-Chef Gordon Videos
     var key = "AIzaSyCJSImhpn0LjuUOCoOW2gbZqEB4KxMeguU";
     var playlistId = 'PLTzMGnJjrsSyDJU9XClzZtuJ6GAIsvRk7';
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
@@ -83,57 +119,29 @@ $(document).ready(function () {
 
     function loadVids() {
         $.getJSON(URL, options, function (data) {
-            var id = data.items[0].snippet.resourceId.videoId;
-            console.log(id);
-            //console.log(data.items[0].snippet.resourceId.videoId);
             video1 = data.items[1].snippet.resourceId.videoId;
             title1 = data.items[1].snippet.title;
-            desc1 = data.items[1].snippet.description.substring(0, 100);
 
             video2 = data.items[2].snippet.resourceId.videoId;
             title2 = data.items[2].snippet.title;
-            desc2 = data.items[2].snippet.description.substring(0, 100);
-
-            video3 = data.items[3].snippet.resourceId.videoId;
-            title3 = data.items[3].snippet.title;
-            desc3 = data.items[3].snippet.description.substring(0, 100);
 
             video4 = data.items[4].snippet.resourceId.videoId;
             title4 = data.items[4].snippet.title;
-            desc4 = data.items[4].snippet.description.substring(0, 100);
-
-            //   video3 = console.log(data.items[3].snippet.resourceId.videoId);
-            //   video4 = console.log(data.items[4].snippet.resourceId.videoId);
-            //  console.log(id);
 
             var vidURL = "https://www.youtube.com/embed/" + video1;
             $("#frame").attr("src", vidURL);
-            $("#vid-desc1").text(title1);
+            $("#vid-desc").text(title1);
 
             var vidURL2 = "https://www.youtube.com/embed/" + video2;
             $("#frame2").attr("src", vidURL2);
             $("#vid-desc2").text(title2);
 
-            var vidURL3 = "https://www.youtube.com/embed/" + video3;
-            $("#frame3").attr("src", vidURL3);
-            $("#vid-desc3").text(title3);
-
             var vidURL4 = "https://www.youtube.com/embed/" + video4;
-            $("#frame4").attr("src", vidURL4);
-            $("#vid-desc4").text(title4);
+            $("#frame3").attr("src", vidURL4);
+            $("#vid-desc3").text(title4);
 
-
-            mainVid(id);
-            // resultsLoop(data);
         });
-
-        function mainVid(id) {
-            $('#video').html(`
-    				<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    			`);
-        }
     }// loadVids function
-
 
 
 });
